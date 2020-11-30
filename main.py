a5039c0472517459e0b06c1b76d990f428053c52
dariogeffen <dariogeffen@web.de>
2020-11-30 17:59:14 +0100

    v3

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == '__main__':
     app.run()  # if you use the port parameter, delete it before deploying to Heroku
